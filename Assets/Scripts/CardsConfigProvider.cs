a8968b739d7e7172bd7a04c3ab51911dd46c86b5
Dan <danildanilchenko.27@gmail.com>
2025-04-09 19:35:07 +0700

    cards config provider

---
+++ Assets/Scripts/CardsConfigProvider.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using DefaultNamespace.Models.CardsConfig;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CardsConfigProvider
+    {
+        private readonly Dictionary<int, Sprite> _spritesDictionary;
+        
+        public CardsConfigProvider(CardsConfig cardsConfig)
+        {
+            _spritesDictionary = new Dictionary<int, Sprite>();
+            
+            foreach (var card in cardsConfig.Cards)
+            {
+                _spritesDictionary.Add(card.Id, card.Sprite);
+            }
+        }
+        
+        public Sprite GetSpriteById(int id)
+        {
+            return _spritesDictionary[id];
+        }
+    }
+}
